153d9250c1db1c8bbbb834495b977e6cae5f30c2
App Academy Student <student@appacademy.io>
2013-11-05 10:42:33 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eatloco::Application.config.secret_token = '733b755790ba2b5b7429e64de0d24f14ab26a2fd01d82cfc1d3605be383a667d75e70508c33c0916a3eb06b793a8cf730d9024dc0f4e5495102dd79d0669e380'
