65030ff877431ebb84aa4055b02c427702530d0a
Varun Mangla <manglav@gmail.com>
2013-11-06 18:11:01 -0800

    Initial order and datetimepicker created.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,23 @@
 class Order < ActiveRecord::Base
   attr_accessible :delivery_date, :delivery_type, :desired_price, :expiration_date, :g_weight, :notes, :parent_id, :user_id, :winner_id
 
+
+  #VALIDATIONS LIST
+
+  #delivery type must be one of three options, delivery, pickup, neutral
+
+
+
+  #IF A NEW ORDER
+  #validate that parent_id is NIL
+  #winner id is NIL
+  # Now to Decision date must be between 2 and 7 days
+  # delivery date must between 12h to 7 days after decision date
+
+  #IF A COUNTER_ORDER
+  # validates NOW to decision day is at zer0
+  #winner id is NIL
+
   belongs_to :user
 
   belongs_to(
@@ -23,4 +40,9 @@ class Order < ActiveRecord::Base
   foreign_key: :winner_id,
   primary_key: :id
   )
+
+
+  def delivery_datepicker(date_string)
+  end
+
 end
