d58d725511819114e72174f25a2bbb5e191b7d4d
Varun Mangla <manglav@gmail.com>
2013-11-07 17:20:57 -0800

    Fixed attachment requiring size - filepicker does not always return it.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :uploader_id, :filename, :size, :filetype, :url
 
-  validates :filename, :size, :filetype, :url, presence: true
+  validates :filename, :filetype, :url, presence: true
 
   belongs_to :uploader, class_name: "User", foreign_key: :uploader_id
 
